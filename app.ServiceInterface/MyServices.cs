e5940cd64575a7963fcea170a50e98e8f313dac8
JaiRam <praveengandhi.p@gmail.com>
2018-11-10 10:48:58 +0530

    ss

---
+++ app.ServiceInterface/MyServices.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ServiceStack;
+using app.ServiceModel;
+
+namespace app.ServiceInterface
+{
+    public class MyServices : Service
+    {
+        public object Any(Hello request)
+        {
+            return new HelloResponse { Result = $"Hello, {request.Name}!" };
+        }
+    }
+}
--- app/Startup.cs
+++ app/Startup.cs
@@ -1,38 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Funq;
+using ServiceStack;
+using ServiceStack.Configuration;
+using app.ServiceInterface;
 
 namespace app
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
         public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration) => Configuration = configuration;
 
         // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
-
-
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,21 +27,25 @@ namespace app
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                app.UseHsts();
-            }
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseCookiePolicy();
-
-            app.UseMvc(routes =>
+            app.UseServiceStack(new AppHost
             {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                AppSettings = new NetCoreAppSettings(Configuration)
+            });
+        }
+    }
+
+    public class AppHost : AppHostBase
+    {
+        public AppHost() : base("app", typeof(MyServices).Assembly) { }
+
+        // Configure your AppHost with the necessary configuration and dependencies your App needs
+        public override void Configure(Container container)
+        {
+            SetConfig(new HostConfig
+            {
+                DefaultRedirectPath = "/metadata",
+                DebugMode = AppSettings.Get(nameof(HostConfig.DebugMode), false)
             });
         }
     }
